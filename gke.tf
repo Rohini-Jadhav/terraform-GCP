034ae69eb6e49806fe7fa92d629dddf0e2bb24c3
Rohini-Jadhav <rjrohini.jadhav@gmail.com>
2022-07-04 18:42:16 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -19,7 +19,6 @@ resource "google_container_cluster" "primary" {
   location = var.region
   
  
-  # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
 
@@ -27,24 +26,18 @@ resource "google_container_cluster" "primary" {
   subnetwork = google_compute_subnetwork.subnet.name
 }
 
-# Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+ 
 
     labels = {
       env = var.project_id
     }
 
-    # preemptible  = true
     machine_type = "n1-standard-1"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
